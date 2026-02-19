c171963f8e5147348bad4fa2f193cdf7fd71163b
jsanchez-codes <julian.sanchez@bsu.edu>
2026-02-19 22:20:38 +0000

    Updated Address Header

--- address.h
+++ address.h
@@ -1,7 +1,7 @@
-#include <string>
-
 #ifndef ADDRESS_H_EXISTS
 #define ADDRESS_H_EXISTS
+
+#include <string>
 
 class Address{
 	private:
@@ -11,7 +11,7 @@ class Address{
 		std::string zip;
 	public:
 		Address();
-		void init(std::string street, city, state, zip);
+		void init(std::string street, std::string city, std::string state, std::string zip);
 		void printAddress();
 };
 
