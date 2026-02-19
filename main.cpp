c1099425dbee8ce898f401125ecdb4487d5cb386
jsanchez-codes <julian.sanchez@bsu.edu>
2026-02-19 02:53:37 +0000

    Updated Main Code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include "date.h"
+#include "date.h"
 //#include "address.h"
 //#include "student.h"
 
@@ -10,7 +10,7 @@ void testStudent();
 int main(){
   std::cout << "Hello!" << std::endl;
   //testAddress();
-  //testDate();
+  testDate();
   //testStudent();
   return 0;
 } // end main
@@ -21,13 +21,13 @@ void testAddress(){
   a.init("123 W Main St", "Muncie", "IN", "47303");
   a.printAddress();
 } // end testAddress
-
+*/
 void testDate(){
  Date d;
  d.init("01/27/1997");
  d.printDate();
 } // end testDate
-
+/*
 void testStudent(){
   std::string studentString = "Danielle,Johnson,32181 Johnson Course Apt. 389,New Jamesside,IN,59379,02/17/2004,05/15/2027,65";
   Student* student = new Student();
